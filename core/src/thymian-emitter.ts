188aca0605ddc9901a575c31bd33399565e6d328
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2025-01-13 21:27:52 +0100

    feat: make methods private

--- core/src/thymian-emitter.ts
+++ core/src/thymian-emitter.ts
@@ -16,34 +16,40 @@ export type ThymianEmitterOptions = {
 export type ThymianEvent = 'thymian.ready' | 'thymian.close';
 
 export class ThymianEmitter {
-  private timeoutHandler: NodeJS.Timeout | undefined;
-  private timeoutResolve!: () => void;
-  private readonly timeoutPromise: Promise<void>;
+  #timeoutHandler: NodeJS.Timeout | undefined;
+  #timeoutResolve!: () => void;
+  readonly #timeoutPromise: Promise<void>;
+  readonly #logger: Logger;
+  readonly #emitter: EventEmitter2;
+  readonly #options: ThymianEmitterOptions;
 
   constructor(
-    private readonly logger: Logger,
-    private readonly options: ThymianEmitterOptions = { timeout: 1000 },
-    private readonly emitter = new EventEmitter2({
+    logger: Logger,
+    options: ThymianEmitterOptions = { timeout: 1000 },
+    emitter = new EventEmitter2({
       wildcard: true,
       delimiter: '.',
       maxListeners: 100,
     })
   ) {
-    this.timeoutPromise = new Promise((resolve) => {
-      this.timeoutResolve = resolve;
+    this.#logger = logger;
+    this.#options = options;
+    this.#emitter = emitter;
+    this.#timeoutPromise = new Promise((resolve) => {
+      this.#timeoutResolve = resolve;
     });
-    this.resetTimeout();
+    this.#resetTimeout();
   }
 
   on(event: string, listener: ThymianListerFn): void {
-    this.emitter.on(event, listener);
+    this.#emitter.on(event, listener);
   }
 
   onAsync<T>(
     event: string | ThymianEvent,
     listener: ThymianListerAsyncFn<T>
   ): void {
-    this.emitter.on(event, listener, {
+    this.#emitter.on(event, listener, {
       async: true,
       promisify: true,
       nextTick: false,
@@ -59,19 +65,19 @@ export class ThymianEmitter {
   }
 
   emit(event: string, ...args: unknown[]): void {
-    this.resetTimeout();
-    this.logger.debug(`Emitting event "${event}".`);
-    this.emitter.emit(event, ...args);
+    this.#resetTimeout();
+    this.#logger.debug(`Emitting event "${event}".`);
+    this.#emitter.emit(event, ...args);
   }
 
   emitAsync<T = unknown>(event: string, ...args: unknown[]): Promise<T[]> {
-    this.resetTimeout();
-    this.logger.debug(`Emitting event "${event}".`);
-    return this.emitter.emitAsync(event, ...args) as Promise<T[]>;
+    this.#resetTimeout();
+    this.#logger.debug(`Emitting event "${event}".`);
+    return this.#emitter.emitAsync(event, ...args) as Promise<T[]>;
   }
 
   once(event: string, listener: ThymianListerFn): void {
-    this.emitter.once(event, listener, {
+    this.#emitter.once(event, listener, {
       async: true,
       promisify: true,
       nextTick: false,
@@ -79,23 +85,23 @@ export class ThymianEmitter {
   }
 
   public onIdle(): Promise<void> {
-    return this.timeoutPromise;
+    return this.#timeoutPromise;
   }
 
   // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   onAny(listener: (event: string | string[], ...args: any[]) => any): void {
-    this.emitter.onAny(listener);
+    this.#emitter.onAny(listener);
   }
 
-  private resetTimeout(): void {
-    this.logger.debug('Reset timeout.');
-    if (this.timeoutHandler) {
-      clearTimeout(this.timeoutHandler);
+  #resetTimeout(): void {
+    this.#logger.debug('Reset timeout.');
+    if (this.#timeoutHandler) {
+      clearTimeout(this.#timeoutHandler);
     }
 
-    this.timeoutHandler = setTimeout(
-      this.timeoutResolve,
-      this.options.timeout ?? 1000
+    this.#timeoutHandler = setTimeout(
+      this.#timeoutResolve,
+      this.#options.timeout ?? 1000
     );
   }
 }
