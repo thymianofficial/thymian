1e9ab7f80337dfefe9c5e6ad6bc361a6cda8facc
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2025-07-16 19:54:43 +0200

    ignore packages

--- libs/rfc-9110-rules/eslint.config.mjs
+++ libs/rfc-9110-rules/eslint.config.mjs
@@ -9,7 +9,7 @@ export default [
         'error',
         {
           ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'],
-          ignoredDependencies: ['vitest']
+          ignoredDependencies: ['vitest', '@thymian/test-utils']
 
         },
       ],
