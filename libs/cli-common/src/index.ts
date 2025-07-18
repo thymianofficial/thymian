dabf5b5f8ec1c0baab1f6328e9724ed38128ffd2
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2025-07-18 11:05:30 +0200

    rename file

--- libs/cli-common/src/index.ts
+++ libs/cli-common/src/index.ts
@@ -1,7 +1,7 @@
 export * from './base-cli-run-command.js';
 export * from './default-config.js';
 export * as oclif from './oclif.js';
-export * from './plugin-init-hook.js';
+export * from './plugin-hook.js';
 export * as prompts from './prompts.js';
 export * from './read-plugins.js';
 export * from './thymian-config.js';
