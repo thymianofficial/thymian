42e346259e0b3b29b75fbe063424f08b8ce2aaf5
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2026-01-02 08:56:53 +0100

    feat(cli): add command to generate schema for config file

---
+++ packages/thymian/src/commands/schema.ts
@@ -0,0 +1,35 @@
+import { BaseCliRunCommand, thymianConfigSchema } from '@thymian/cli-common';
+
+const configSchema = {
+  ...thymianConfigSchema,
+};
+
+export default class GenerateConfigSchema extends BaseCliRunCommand<
+  typeof GenerateConfigSchema
+> {
+  override async run(): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    configSchema.properties.plugins['properties'] = {};
+
+    for (const { plugin } of this.thymian.plugins) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      configSchema.properties.plugins['properties'][plugin.name] = {
+        type: 'object',
+        required: ['options'],
+        properties: {
+          path: {
+            type: 'string',
+          },
+          verbose: {
+            type: 'boolean',
+          },
+          options: plugin.options,
+        },
+      };
+    }
+
+    this.log(JSON.stringify(configSchema, null, 2));
+  }
+}
