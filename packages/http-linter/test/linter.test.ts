cc53bf04195c452810dfd5d4ebc308f8c9f234a9
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2026-01-04 16:50:06 +0100

    test(http-linter): add test for duplicated rule name

---
+++ packages/http-linter/test/linter.test.ts
@@ -0,0 +1,54 @@
+import { NoopLogger, ThymianFormat } from '@thymian/core';
+import type { JSONSchemaType } from '@thymian/core/ajv';
+import { describe, expect, it } from 'vitest';
+
+import type { Rule, RuleMeta } from '../src/index.js';
+import { AbstractLinter } from '../src/linter/abstract-linter.js';
+
+describe('AbstractLinter', () => {
+  class TestLinter extends AbstractLinter {
+    override runRule<Options extends Record<string, unknown>>(
+      rule: Rule,
+      options: Options,
+    ): Promise<boolean> {
+      return Promise.resolve(false);
+    }
+  }
+
+  it('should throw error for duplicated rule', () => {
+    const ruleMeta: RuleMeta<never> = {
+      name: 'abc',
+      type: [],
+      options: {} as JSONSchemaType<never>,
+      severity: 'off',
+    };
+
+    expect(
+      () =>
+        new TestLinter(
+          new NoopLogger(),
+          [
+            {
+              meta: {
+                ...ruleMeta,
+              },
+            },
+            {
+              meta: {
+                ...ruleMeta,
+              },
+            },
+            {
+              meta: {
+                ...ruleMeta,
+                name: 'cba',
+              },
+            },
+          ],
+          () => undefined,
+          new ThymianFormat(),
+          {},
+        ),
+    ).toThrowError('Duplicate rule names found: abc');
+  });
+});
