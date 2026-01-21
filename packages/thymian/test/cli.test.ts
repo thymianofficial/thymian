8d90ef00c73889049e20a3b94cf8d05634fc633f
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2026-01-21 12:09:44 +0100

    fix(cli): use dirname instead of cwd

---
+++ packages/thymian/test/cli.test.ts
@@ -0,0 +1,19 @@
+import { execSync } from 'node:child_process';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { describe, expect, it } from 'vitest';
+
+describe('cli', () => {
+  it.each([['run'], ['dev']])(
+    '%s.js can be called from any current working directory',
+    async (command) => {
+      expect(() =>
+        execSync(`../bin/${command}.js http-linter:overview`, {
+          stdio: 'pipe',
+          cwd: dirname(fileURLToPath(import.meta.url)),
+        }),
+      ).not.toThrow();
+    },
+  );
+});
