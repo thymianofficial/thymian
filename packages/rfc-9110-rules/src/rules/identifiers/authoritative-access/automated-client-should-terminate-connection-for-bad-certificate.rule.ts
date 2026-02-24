d08c6fd560bb3b52cdc9500d11960a6847ea86d2
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2026-02-24 13:36:37 +0100

    fix(rfc-9110-rules): fix severity level

--- packages/rfc-9110-rules/src/rules/identifiers/authoritative-access/automated-client-should-terminate-connection-for-bad-certificate.rule.ts
+++ packages/rfc-9110-rules/src/rules/identifiers/authoritative-access/automated-client-should-terminate-connection-for-bad-certificate.rule.ts
@@ -3,7 +3,7 @@ import { httpRule } from '@thymian/http-linter';
 export default httpRule(
   'rfc9110/automated-client-should-terminate-connection-for-bad-certificate',
 )
-  .severity('error')
+  .severity('warn')
   .type('informational')
   .url(
     'https://www.rfc-editor.org/rfc/rfc9110.html#name-https-certificate-verificat',
