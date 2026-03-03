fdb7811abf550823916fbaa732255eed0bee34aa
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2026-03-03 14:42:10 +0100

    fix(rfc-9110-rules): change rule type to informational

--- packages/rfc-9110-rules/src/rules/message-abstraction/control-data/server-must-not-send-non-conformant-version.rule.ts
+++ packages/rfc-9110-rules/src/rules/message-abstraction/control-data/server-must-not-send-non-conformant-version.rule.ts
@@ -2,7 +2,7 @@ import { httpRule } from '@thymian/http-linter';
 
 export default httpRule('rfc9110/server-must-not-send-non-conformant-version')
   .severity('error')
-  .type('static', 'analytics', 'test', 'informational')
+  .type('informational')
   .appliesTo('server')
   .url('https://www.rfc-editor.org/rfc/rfc9110.html#section-6.2')
   .description(
