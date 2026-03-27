57aa0c681e28df4e651a27b3d7166283b43993a7
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2026-03-27 12:33:33 +0100

    refactor(api-description-validation-rules): use thymian name scope

--- packages/api-description-validation-rules/src/index.ts
+++ packages/api-description-validation-rules/src/index.ts
@@ -1,7 +1,7 @@
 import type { RuleSet } from '@thymian/core';
 
 const apiDescriptionValidation: RuleSet = {
-  name: 'api-description-validation',
+  name: '@thymian/api-description-validation-rules',
   pattern: 'rules/**/*.rule.js',
 };
 
