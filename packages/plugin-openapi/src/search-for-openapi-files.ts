0e490da527721b807d7eccbf2f2763cada0f44a5
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2026-04-08 14:53:14 +0200

    fix(plugin-openapi): make openapi search regexp more strict

--- packages/plugin-openapi/src/search-for-openapi-files.ts
+++ packages/plugin-openapi/src/search-for-openapi-files.ts
@@ -32,8 +32,8 @@ export async function searchForOpenApiFiles(cwd: string): Promise<string[]> {
 
       for await (const line of rl) {
         if (
-          /^\s*"?swagger"?\s*:\s*("?2\.0"?)/i.test(line) ||
-          /^\s*"?openapi"?\s*:\s*("?3\.([01])\.\d+"?)/i.test(line)
+          /^\s*"?swagger"?\s*:\s*"?2\.0(?:\.0)?"?\s*$/i.test(line) ||
+          /^\s*"?openapi"?\s*:\s*"?3\.[01]\.\d+"?\s*$/i.test(line)
         ) {
           found.push(match);
           break;
